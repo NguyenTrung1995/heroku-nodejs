dd7715116608d818e39169fc88c16a2d1929a28f
Trung Nguyen <trungkama1995nqd@gmail.com>
2020-10-06 12:13:35 +0700

    refactor

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/cart/list", (req, res) => {
+    const list = [
+        {
+            id: 1,
+            name: 'Ahihi',
+        },
+        {
+            id: 2,
+            name: 'Hehe',
+        }
+    ];
+    res.send(list);
+  });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
